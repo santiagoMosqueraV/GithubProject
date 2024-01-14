be28bdffffa45bf29ed1aa12d7266fad116e6413
santiagoMosqueraV <krakedev2@gmail.com>
2024-01-14 15:13:53 -0500

    commit desde Github

---
+++ ejercicio.js
@@ -0,0 +1,8 @@
+
+saludar= function () {
+    let cmpImagenSaludo;
+    cmpImagenSaludo= document.getElementById("imgSaludo")
+    cmpImagenSaludo.src="./imagenes/krusty.gif"
+    console.log("ingresa a saludar");
+
+}
